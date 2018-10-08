4b4590da72b47a39f61cab0356a154ead87ace00
Hatem <hatem.saturi@gmail.com>
2018-10-08 09:53:34 +0200

    1 ) ajout d'un projet test avec suffixe TEST, Ca fonctionne par attribut [TestClass]

---
+++ ConsolePour TestTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ConsolePour TestTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsolePour TestTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("435dc22c-1c03-4b90-9b8d-a460ab5c21c0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
