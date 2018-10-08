6d5d7974742ffc730aab90a97f46e9bd588d672c
Hatem <hatem.saturi@gmail.com>
2018-10-08 10:00:10 +0200

    2) Creations de deux  methode de test (listées et executées dans Test/fenetre/explorateur de test)

--- ConsolePour TestTests/UnitTest1.cs
+++ ConsolePour TestTests/UnitTest1.cs
@@ -9,6 +9,20 @@ namespace ConsolePour_TestTests
         [TestMethod]
         public void TestMethod1()
         {
+            int i = 1;
+            int j = 2;
+            Assert.AreEqual(3, i + j); 
+            //i+j doit etre egal à 3
+        }
+
+        [TestMethod]
+        public void TestMethodAge()
+        {
+            var dateNaissance = new DateTime(2010, 5, 1);
+
+            Assert.IsTrue(
+                DateTime.Today.Subtract(dateNaissance).Days / 365 > 10); 
+            // age doit etre plus de 10ans
         }
     }
 }
